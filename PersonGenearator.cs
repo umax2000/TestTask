9b7d79183f25c1bef4808626030ec36267d58812
umax2000 <78012863+umax2000@users.noreply.github.com>
2022-09-20 18:25:28 +0300

    Add files via upload

---
+++ PersonGenearator.cs
@@ -0,0 +1,124 @@
+﻿using static ConsoleApp4.Program;
+using System.Collections.Generic;
+using System;
+using System.Text;
+
+namespace Collection.Generator
+{
+
+    internal class PersonsGenerator
+    {
+        public List<Person> persons = new List<Person>();
+        static Random rand = new Random();
+        public static double averageChildAge;
+        public PersonsGenerator(int count)
+        {
+            for (int i = 0; i < count; ++i)
+            {
+                persons.Add(new Person
+                {
+                    Id = i,
+                    //решил использовать Faker для упрощения генерации некоторых переменных
+                    TransportId = Guid.NewGuid(),
+                    FirstName = Faker.Name.First(),
+                    LastName = Faker.Name.Last(),
+                    //честно говоря, не знаю что такое SequenceID, в интернете не смог ничего найти
+                    SequenceId = i,
+                    CreditCardNumbers = GetRandomCardNumber(),
+                    Age = rand.Next(21, 65),
+                    Phones = GetRandomTelNumber(),
+                    birthDate = GetRandomAdultBirthDate(),
+                    Salary = rand.Next(10000, 100000),
+                    IsMarred = Faker.Boolean.Random(),
+                    Gender = Faker.Enum.Random<Gender>(),
+                    Children = CreateChild(),
+                }); ;
+            }
+            int childCount = 0;
+            int allChildYears = 0;
+            for (int i = 0; i < count; ++i)
+            {              
+                if(persons[i].Children != null)
+                {
+                    childCount = childCount + persons[i].Children.Length;
+                    for (int j = 0; j < persons[i].Children.Length; ++j)
+                    {
+                        if (persons[i].Children[j] != null)
+                        {
+                            allChildYears = allChildYears + (2022 - persons[i].Children[j].BirthDate.Year);
+                        }
+                    }
+                }
+            }
+            if(childCount != 0)
+            {
+                averageChildAge = (double)(allChildYears / childCount);
+            }
+        }
+        public string GetRandomTelNumber()
+        {
+            StringBuilder telNumber = new StringBuilder(13);
+            int number;
+            telNumber = telNumber.Append("+7 ");
+            for (int i = 0; i < 2; i++)
+            {
+                number = rand.Next(100, 999);
+                telNumber.Append(number.ToString() + " ");
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                number = rand.Next(10, 99);
+                telNumber.Append(number.ToString() + " ");
+            }
+            return telNumber.ToString();
+        }
+        public BirthDate GetRandomAdultBirthDate()
+        {
+            BirthDate birthDate = new BirthDate();
+            birthDate.Day = rand.Next(0, 31);
+            birthDate.Month = rand.Next(0, 12);
+            birthDate.Year = rand.Next(1957, 2001);
+            return birthDate;
+
+        }
+        public BirthDate GetRandomAChildBirthDate()
+        {
+            BirthDate birthDate = new BirthDate();
+            birthDate.Day = rand.Next(0, 31);
+            birthDate.Month = rand.Next(0, 12);
+            //огрничим возраст шестью годами
+            birthDate.Year = rand.Next(2016, 2022);
+            return birthDate;
+
+        }
+        public string GetRandomCardNumber()
+        {
+            StringBuilder CardNumber = new StringBuilder(20);
+            int number;
+            for (int i = 0; i < 4; i++)
+            {
+                number = rand.Next(1000, 9999);
+                CardNumber.Append(number.ToString() + " ");
+            }
+            return CardNumber.ToString();
+        }
+        public Child[] CreateChild()
+        {
+            //Ограничим количество детей двумя
+            int childCount = rand.Next(0, 2);
+            if(childCount == 0)
+                return null;
+            Child[] Children = new Child[childCount];
+            for (int i = 0; i < childCount; ++i)
+            {
+                Children[i] = new Child();
+                Children[i].Id = i;
+                Children[i].FirstName = Faker.Name.First();
+                Children[i].LastName = Faker.Name.Last();
+                Children[i].BirthDate = GetRandomAChildBirthDate();
+                Children[i].Gender = Faker.Enum.Random<Gender>();
+            }
+            return Children;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using Collection.Generator;
+using Boolean = System.Boolean;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public Int32 Id { get; set; }
+            public Guid TransportId { get; set; }
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+            public Int32 SequenceId { get; set; }
+            public String CreditCardNumbers { get; set; }
+            public Int32 Age { get; set; }
+            public String Phones { get; set; }
+            //не получилось нормальным образом преобразовать POSIX в формат даты
+            //поэтому решил использовать структуру для удобства)
+            public BirthDate birthDate { get; set; }
+            public Double Salary { get; set; }
+            public Boolean IsMarred { get; set; }
+            public Gender Gender { get; set; }
+            public Child[] Children { get; set; }
+        }
+        public class Child
+        {
+            public Int32 Id { get; set; }
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+            public BirthDate BirthDate { get; set; }
+            public Gender Gender { get; set; }
+        }
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+        public struct BirthDate
+        {
+            public int Day, Month, Year;
+        }
+        static void Main(string[] args)
+        {
+            PersonsGenerator generator = new PersonsGenerator(10);
+            //не понятно где именно нужно использовать lowerCamelCase потому что у нас все переменные однословные
+            //решил просто использовать функцию ToLower
+            File.WriteAllText("Persons.json", JsonConvert.SerializeObject(generator));
+
+            generator.persons.Clear();
+
+            var persons = JsonConvert.DeserializeObject<PersonsGenerator>(File.ReadAllText("Persons.json"));
+
+            Console.WriteLine($"Persons count is {persons.persons.Count}");
+            // в задании указано вывести количество карт но нигде не указано что их количество может быть отличным от единицы,
+            // в предоставленной модели есть только серийный номер карты но не количество 
+            Console.WriteLine($"Credit cards count is {persons.persons.Count}");
+            Console.WriteLine($"Average children age is {PersonsGenerator.averageChildAge}");
+
+            Console.ReadKey();
+        }
+    }
+}
